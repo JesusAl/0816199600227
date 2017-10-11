1da9805074d01db14b3598eeb3b74735a1adf690
JesusAl <lainez_jesus13@hotmail.com>
2017-10-10 19:17:09 -0600

    Commit

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/cuenta', function(req, res, next) {
+  res.render('cuenta', {});
+}):
+
+
 module.exports = router;
